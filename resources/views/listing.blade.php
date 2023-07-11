c6559a59ca113bc645a33bc0cab78dbf58804d36
Spyabo <reesewright02@protonmail.com>
2023-07-11 13:15:31 +0100

    Add search and hero components

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,3 +1,11 @@
+@extends('layout')
+
+@section('content')
+
+@include('partials._search')
+
 <h2>{{$gig['title']}}</h2>
 
 <p>{{$gig['description']}}</p>
+
+@endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 
+@include('partials._hero')
+@include('partials._search')
+
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 @if(empty($gigs))
@@ -18,7 +21,7 @@
         />
         <div>
             <h3 class="text-2xl">
-                <a href="show.html">{{ $gig->title }}</a>
+                <a href="/listings/{{ $gig->id }}">{{ $gig->title }}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{ $gig->company }}</div>
             <ul class="flex">
