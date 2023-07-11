81c4c1f36e5cc2fad952c9b1d6493fad4f98c661
Spyabo <reesewright02@protonmail.com>
2023-07-11 13:08:21 +0100

    Add html and tailwind templates

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,15 +2,53 @@
 
 @section('content')
 
-<h1>{{($heading)}}</h1>
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 @if(empty($gigs))
     <p>No listings found.</p>
 @endif
 
 @foreach ($gigs as $gig)
-    <h2><a href="/listings/{{$gig['id']}}">{{$gig['title']}}</a></h2>
-    <p>{{$gig['description']}}</p>
+<div class="bg-gray-50 border border-gray-200 rounded p-6">
+    <div class="flex">
+        <img
+            class="hidden w-48 mr-6 md:block"
+            src="{{asset('images/no-image.png')}}"
+            alt=""
+        />
+        <div>
+            <h3 class="text-2xl">
+                <a href="show.html">{{ $gig->title }}</a>
+            </h3>
+            <div class="text-xl font-bold mb-4">{{ $gig->company }}</div>
+            <ul class="flex">
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Laravel</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">API</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Backend</a>
+                </li>
+                <li
+                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                >
+                    <a href="#">Vue</a>
+                </li>
+            </ul>
+            <div class="text-lg mt-4">
+                <i class="fa-solid fa-location-dot"></i> {{ $gig->location }}
+            </div>
+        </div>
+    </div>
+</div>
 @endforeach
 
 @endsection
