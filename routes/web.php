d0bdfaa3b1b80c24f09263865c72611ca62224d4
Spyabo <reesewright02@protonmail.com>
2023-07-11 14:30:10 +0100

    Remove unused heading tag

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('listings', [
-        'heading' => 'Listings',
         'gigs' => Listing::all(),
     ]);
 });
