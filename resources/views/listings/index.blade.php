822aeff95b24a5292e0568cb93ed9df5a57676ff
Spyabo <reesewright02@protonmail.com>
2023-07-18 11:16:36 +0100

    Pagination

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -7,12 +7,18 @@
 
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
-@if(empty($gigs))
-    <p>No listings found.</p>
-@endif
+    @if(empty($gigs))
+        <p>No listings found.</p>
+    @endif
 
-@foreach ($gigs as $gig)
-    <x-listing-card :gig="$gig" />
-@endforeach
+    @foreach ($gigs as $gig)
+        <x-listing-card :gig="$gig" />
+    @endforeach
+
+</div>
+
+<div class="mt-4">
+    {{ $gigs->links() }}
+</div>
 
 @endsection
